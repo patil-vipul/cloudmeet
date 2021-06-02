4fd479bcc2b66e6b4092d4e3bb3ac1c68669029d
Vipul Patil <developer.vipulpatil@gmail.com>
2021-06-02 13:56:15 +0530

    REfactor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
 const app = express();
-const port = 5555;
+const port = 443;
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
